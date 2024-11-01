0e3c56770f297a676425a8ab9976a2975b7abe42
EC2 Default User <ec2-user@ip-172-31-18-222.ap-northeast-1.compute.internal>
2024-11-01 09:08:10 +0000

    Finish ouyoukadai5

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   has_many :post_comments, dependent: :destroy
   has_many :relation_ships, foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relation_ships, source: :followed
-  
+
   has_many :reverse_of_relationships, class_name: "RelationShip", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_one_attached :profile_image
@@ -22,8 +22,22 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def is_followed_by?(user)
     reverse_of_relationships.find_by(follower_id: user.id).present?
+  end
+
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+    elsif search == "forward_match"
+      @user = User.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @user = User.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     end
     resource :relation_ships, only: [:create,:destroy]
   end
+  get "search" => "searches#search", as: "search"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
