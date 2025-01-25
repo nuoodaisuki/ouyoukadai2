5b90397f692c2551572ab59c13a079bacc5c2bc4
EC2 Default User <ec2-user@ip-172-31-37-191.ap-northeast-1.compute.internal>
2025-01-25 16:15:17 +0000

    日ごとの投稿数検索とその非同期化

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
+    get "search" => "users#search"
     member do
       get :followings, :followers
     end
